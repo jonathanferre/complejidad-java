5ac61e4ea035eb90c67af756aec447041c5116bd
Jonathan <jonathanferre2@gmail.com>
2016-10-27 16:26:19 -0300

    Conectar y agregar ciudad

--- src/tp_final/Grafo.java
+++ src/tp_final/Grafo.java
@@ -1,17 +1,47 @@
 package tp_final;
 
 import java.util.ArrayList;
+import java.util.Queue;
 
 public class Grafo {
 	
 	private ArrayList<Ciudad> listaDeCiudades = new ArrayList<Ciudad>();
 	
-	private void agregar_ciudad(Ciudad ciudad, Ruta ruta){
-		
+	public ArrayList<Ciudad> getListaDeCiudades() {
+		return listaDeCiudades;
+	}
+
+	public void setListaDeCiudades(ArrayList<Ciudad> listaDeCiudades) {
+		this.listaDeCiudades = listaDeCiudades;
+	}
+
+	private void agregar_ciudad(Ciudad c){
+		listaDeCiudades.add(c);
 	}
 	
-	private void conectar(){
-		
+	private void conectar(Ciudad origen, Ciudad destino, int pesoMaximo, float velocidadMaxCiudades, int distanciaRuta){
+		Ruta ruta = new Ruta();
+		ruta.setPesoMaximo(pesoMaximo);
+		ruta.setDistancia(distanciaRuta);
+		ruta.setVelocidadMaxCiudades(velocidadMaxCiudades);
+		ruta.setDestino(destino);
+		origen.getAdyacentes().add(ruta);
 	}
+	
+	//recorrer grafos
+	public void bfs(Ciudad origen){
+		ArrayList<Boolean> visitado = new ArrayList<Boolean>();
+		for (int i = 0; i < this.getListaDeCiudades().size(); i++) {
+			visitado.add(false);
+		}
+		return 
+	}
+	
+	
+	private ArrayList<Ciudad> privateBfs(Ciudad origen, Ciudad visitado){
+		Queue<> cola = new Queue
+	}
+	
+	
 
 }
