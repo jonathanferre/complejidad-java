b74f4e220b29572b82b373e9c9de5a4050c0eef5
NicoCav <NicoCav@192.168.1.12>
2016-11-20 19:28:24 -0300

    Se agrego al Main el menu por consola y al RecorredorGrafo se agregaron los 3 DFS

--- src/tp_final/Main.java
+++ src/tp_final/Main.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import clase1_Listas.ListaConArreglo;
 import clase1_Listas.Recorredor;
@@ -15,9 +16,7 @@ public class Main {
 		
 		//crear grafo o llamar inicializar de grafo (pasar grafo por parï¿½metro
 			
-		Grafo g = new Grafo();
-		
-		
+		Grafo g = new Grafo();		
 		
 		
 		//llamar al mï¿½todo que cree la matrï¿½z y conecte
@@ -37,26 +36,105 @@ public class Main {
 		r.comenzar();
 		while(!r.fin()){
 			Ciudad elem = (Ciudad) r.elemento();
-			System.out.println(elem.getDatoCiudad().toString());
+			//System.out.println(elem.getDatoCiudad().toString());
 			
 			//obtengo la/s ruta/s de la ciudad/es
-			System.out.println(elem.getAdyacentes().toString());
+			//System.out.println(elem.getAdyacentes().toString());
 			
 			//Verificar si la ruta estÃ¡ yendo a la ciudad indicada
-			System.out.println(elem.getAdyacentes().get(1).getDestino().getDatoCiudad().getNombreCiudad());
+			//System.out.println(elem.getAdyacentes().get(1).getDestino().getDatoCiudad().getNombreCiudad());
 			
 			r.proximo();
 			
 		}
-		//Hacer consultas
+
+		Scanner sc = new Scanner(System.in);
 		
-		//public void  primerConsulta(Ciudad origen, Ciudad destino){
+		
+		System.out.println(">>>Modulo de Consulta>>>");
+		System.out.println("Eliga una de las siguientes opciones");
+		System.out.println("1-Florencio Varela,La Plata");
+		System.out.println("2-Buenos Aires,Avellaneda");
+		System.out.println("3-Mar del Plata,Azul");
+		System.out.println("4-Olavarria,Bolivar");
+		System.out.println("5-Tandil,Junin");
+		System.out.println("6-Florencio Varela,Junin");
+		System.out.println("Su opcion es:");
+		String opcion = sc.nextLine();
+		
+		//Variables vacías
+		Ciudad origen = null;
+		Ciudad destino = null;
+		Ruta camino = null;
+		Ruta mejorCamino = null;
+		
+		
+		if (opcion.equals("1")){
+			origen = (Ciudad) g.getListaDeCiudades().elemento(0);
+			destino = (Ciudad) g.getListaDeCiudades().elemento(1);
 			
-		//}
-		
-		
-		
 		}
+		
+		if (opcion.equals("2")){
+			origen = (Ciudad) g.getListaDeCiudades().elemento(2);
+			destino = (Ciudad) g.getListaDeCiudades().elemento(3);
+			
+		}
+		
+		if (opcion.equals("3")){
+			origen = (Ciudad) g.getListaDeCiudades().elemento(4);
+			destino = (Ciudad) g.getListaDeCiudades().elemento(5);
+			
+		}
+		
+		if (opcion.equals("4")){
+			origen = (Ciudad) g.getListaDeCiudades().elemento(6);
+			destino = (Ciudad) g.getListaDeCiudades().elemento(7);
+			
+		}
+		
+		if (opcion.equals("5")){
+			origen = (Ciudad) g.getListaDeCiudades().elemento(8);
+			destino = (Ciudad) g.getListaDeCiudades().elemento(9);
+			
+		}
+		
+		if (opcion.equals("6")){
+			origen = (Ciudad) g.getListaDeCiudades().elemento(0);
+			destino = (Ciudad) g.getListaDeCiudades().elemento(9);
+			
+		}
+		
+		RecorredorGrafo recorredorGrafo = new RecorredorGrafo();
+
+		System.out.println("Eliga que desea hacer");
+		System.out.println("1-Velocidad máxima que puede alcanzar un vehículo entre una ciudad origen y otradestino.");
+		System.out.println("2-Ruta desde una ciudad origen y otra destino donde la distancia transitada seamínima.");
+		System.out.println("3-Variación de Alturas entre una ciudad origen y todas las ciudades vecinas.");
+		System.out.println("Su opcion es:");
+		String consulta = sc.nextLine();
+		
+		
+		if (consulta.equals("1")){
+			recorredorGrafo.buscar_VelocidadMax(origen, destino, camino, mejorCamino);
+		}
+		
+		if (consulta.equals("2")){
+			recorredorGrafo.buscar_DistanciaMin(origen, destino, camino, mejorCamino);
+		}
+		
+		if (consulta.equals("3")){
+			recorredorGrafo.buscar_VariacionAltura(origen);
+		}
+		
+		
+		
+		
+	
+	
+	
+	
+	}
 }	
     	
 
--- src/tp_final/RecorredorGrafo.java
+++ src/tp_final/RecorredorGrafo.java
@@ -6,34 +6,8 @@ public class RecorredorGrafo {
 	
 	private ArrayList<Ciudad> visitados = new ArrayList<Ciudad>();
 	
-	public void DFS (Grafo gr, Ciudad origen){
-		
-		/**
-		 * el recorrido va--> empieza por el origen
-		 * 1-marca origen como visitado
-		 * 2-elije un adyacente
-		 * 3-marca como visitado ese adyacente
-		 * 4-no tiene mas adyacentes?
-		 * 5-backTracking
-		 * */
-
-		visitados.add(origen);
-		System.out.println("Camino profundidad: -->" + origen.getDatoCiudad());
-		for (Ruta a: origen.getAdyacentes()){
-			//si no esta en la tabla se revisa sino que salte a otro ady
-			if (!visitados.contains(a.getDestino())){
-				if (a.getDestino()!=null){
-					DFS(gr,a.getDestino());
-					}
-				}
-			else{
-				System.err.println("Ya pase por "+a.getDestino().getDatoCiudad());
-				}
-			}
-		}
-	
-	
-	public Ruta buscar_VelocidadMax (Ciudad origen, Ciudad destino, Ruta camino, Ruta mejorCamino){
+			
+	public void buscar_VelocidadMax (Ciudad origen, Ciudad destino, Ruta camino, Ruta mejorCamino){
 		camino.setOrigen(origen);
 		visitados.add(origen);
 		if (origen.equals(destino)){
@@ -48,7 +22,37 @@ public class RecorredorGrafo {
 				}
 			}
 		}
-		return mejorCamino;
+		System.out.println(mejorCamino);
+	}
+	
+	public void buscar_DistanciaMin (Ciudad origen, Ciudad destino, Ruta camino, Ruta mejorCamino){
+		camino.setOrigen(origen);
+		visitados.add(origen);
+		if (origen.equals(destino)){
+			if (camino.getDistancia()<mejorCamino.getDistancia()){
+				mejorCamino=camino;
+				
+			}
+		}else{
+			for (Ruta r: origen.getAdyacentes()){
+				if (!visitados.contains(r)){
+					buscar_VelocidadMax(r.getDestino(), destino, camino, mejorCamino);
+				}
+			}
+		}
+		 System.out.println(mejorCamino);
+	}
+	
+	
+	public int buscar_VariacionAltura (Ciudad origen){
+		int altura = origen.getDatoCiudad().getAltura();
+		
+		for (int i = 0; i < origen.getAdyacentes().size(); i++) {
+			int alturaVar=  origen.getAdyacentes().get(i).getDestino().getDatoCiudad().getAltura();
+		}
+		return altura;
+			
+		
 	}
 	
 	
